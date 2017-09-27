2e58b610fd7c656be8285eec5e2b4df89c2b74d6
Matthew <Matthew@DESKTOP-8D83BKO>
2017-09-27 12:14:54 -0400

    Finished!  Built a complete version.  Need to figure out how to submit to OWL

--- AddressBookTutorial/AddressBookTutorial/MyPerson.cpp
+++ AddressBookTutorial/AddressBookTutorial/MyPerson.cpp
@@ -14,35 +14,41 @@ MyPerson::~MyPerson()
 
 void MyPerson::handleRequest(const Request & request, Response & response)
 {
-	string s = request.method();
-	string x = request.contentType();
-	ostream &out = response.out();
-	//out << s << endl;
-	//out << "/person" << endl;
-	out << s << endl;
-	out << x << endl;
-	/*
-	if (s.compare("/person/") == 0)
+	string s = request.path();
+	string y = request.pathInfo();
+	ostream &out = response.out();	
+	if (s.compare("/person") == 0 && y.length() == 0)
 	{
-	out << "  {" << endl;
-	out << "    \"" << "people" << "\"" << ": [" << endl;
-	for (int i = 0; i < people.size(); i++)
-	{
-
-	out << "      {" << " \"" << "id" << "\"" << ":" << " \"" << i + 1 << "\"" << ", " << people[i].getName().lastNameToJSON() << ", " << people[i].getName().firstNameToJSON() << " }" << endl;
-	}
-	out << "    ]" << endl;
-	out << "  }";
+		out << "  {" << endl;
+		out << "    \"" << "people" << "\"" << ": [" << endl;
+		for (int i = 0; i < people.size(); i++)
+		{
+			out << "      {" << " \"" << "id" << "\"" << ":" << " \"" << i + 1 << "\"" << ", " << people[i].getName().lastNameToJSON() << ", " << people[i].getName().firstNameToJSON() << " }," << endl;
+		}
+		out << "    ]" << endl;
+		out << "  }";
 	}
 
 	else
 	{
-	out << "SOS" << endl;
+		y.erase(0, 1);
+		out << "  {" << endl;
+		out << "    \"" << "id" << "\"" << ":  " << "\"" << stoi(y) << "\"," << endl;
+		try
+		{
+			out << "    " << people.at(stoi(y)-1).getName().lastNameToJSON() << "," << endl;
+			out << "    " << people.at(stoi(y)-1).getName().firstNameToJSON() << "," << endl;
+			out << "    " << people.at(stoi(y)-1).getAddress().streetToJSON() << "," << endl;
+			out << "    " << people.at(stoi(y)-1).getAddress().cityToJSON() << "," << endl;
+			out << "    " << people.at(stoi(y)-1).getAddress().provinceToJSON() << "," << endl;
+			out << "    " << people.at(stoi(y)-1).getAddress().countryToJSON() << "," << endl;
+			out << "    " << people.at(stoi(y)-1).getPhoneNumber().PhoneNumberToJSON() << "," << endl;
+			out << "  }" << endl;
+		}
+		catch (out_of_range& e)
+		{
+			out << "    Out of Range!" << endl;
+			out << "  }" << endl;
+		}
 	}
-	*/
-	
-	
-	
-
-	
 }
